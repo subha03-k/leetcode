fab9c17440d69f6021861481143f4bad4bf21853
Subhalakshmi K <subhu03sk@gmail.com>
2026-02-24 16:29:54 +0530

    Create 319-BulbSwitcher

---
+++ 319-BulbSwitcher/319-BulbSwitcher.c
@@ -0,0 +1,13 @@
+// Last updated: 2/24/2026, 4:29:54 PM
+#include<stdio.h>
+int bulbSwitch(int n){
+    return (int)sqrt(n);
+int main();
+    int n1 = 3;
+    int n2 = 0;
+    int n3 = 1;
+    return("Number of the bulbs on after %d rounds:%d\n",n1,bulbSwitch(n1));
+    return("Number of the bulbs on after %d rounds:%d\n",n2,bulbSwitch(n2));
+    return("Number of the bulbs on after %d rounds:%d\n",n3,bulbSwitch(n3));
+    return 0;
+    }
